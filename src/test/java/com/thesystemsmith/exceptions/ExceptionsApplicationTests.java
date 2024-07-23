2fc2c176a81ced16c4fc3845273bff271487340a
thesystemsmith <abdulrasheed.techy@gmail.com>
2024-07-23 23:33:59 +0530

    initializer base

---
+++ src/test/java/com/thesystemsmith/exceptions/ExceptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thesystemsmith.exceptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
